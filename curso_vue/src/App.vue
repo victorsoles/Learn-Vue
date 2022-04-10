49a68e14d6a678905acfd91b17d20c72d446726a
Victor Soles <victorsoles09@gmail.com>
2022-04-10 09:40:50 -0300

    First Component Vue

---
+++ curso_vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <FirstComponent />
+  <p>Meu Jogo favorito é {{ test }}</p>
+  </div>
+</template>
+
+<script>
+import FirstComponent from './components/FirstComponent.vue'
+
+  export default {
+  components: { FirstComponent },
+    name: 'App',
+    Components: {
+      FirstComponent
+    },
+    data() {
+      return {
+        test: "Among Us"
+      }
+    }
+  }
+</script>
---
+++ curso_vue/src/components/FirstComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Alexandro Marrone</h1>
+        <p>Meu nome é {{ nome }} e sou {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'FirstComponent',
+        data() {
+            return {
+                nome: "Vitin Soles",
+                profissao: "Astronauta"
+            }
+        }
+    }
+</script>
